3a1cff4c4b6dc4c5b9251f3481107345bed47d34
cyberickk <blingblingda@hotmail.com>
2020-08-18 14:07:14 +0800

    update remove

---
+++ gender-features2.py
@@ -0,0 +1,9 @@
+def gender_features2(name):
+    features = {}
+    features["firstletter"] = name[0].lower()
+    features["lastletter"] = name[-1].lower()
+    for letter in 'abcdefghijklmnopqrstuvwxyz':
+        features["count(%s)" % letter] = name.lower().count(letter)
+        features["has(%s)" % letter] = (letter in name.lower())
+
+    return features
